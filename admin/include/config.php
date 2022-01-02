898439cf36993cc6a993689182744bb989b61f57
Rima <91939415+rimariy@users.noreply.github.com>
2022-01-02 10:44:50 +0200

    Add files via upload

---
+++ admin/include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','orange_family');
+
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+defined('CSS') ? null : define('CSS', 'layout' . DS . 'css' . DS);
+defined('JS') ? null : define('JS', 'layout' . DS . 'js' . DS);
---
+++ admin/include/init.php
@@ -0,0 +1,27 @@
+<?php
+
+
+// defined('DS') ? null :define('DS',DIRECTORY_SEPARATOR);
+
+// // define("SITE_ROOT", DS . 'Computer' . DS . "opt".DS.'lampp'.DS.'htdocs'.DS.'gallery-oop');
+
+// // defined('INCLUDES_PATH') ? null : define ('INCLUDES_PATH',SITE_ROOT.DS.'admin'.DS.'includes');
+
+// require_once('function.php');
+// require_once('config.php');
+// require_once('database.php');
+// require_once('db_object.php');
+// require_once('user.php');
+// require_once('photo.php');
+// require_once('session.php');
+// //Data base Constants
+
+// defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+// require_once("include" . DS . "config.php");
+
+// require_once("include" . DS . "function.php");
+
+// require_once("lib" . DS . "database.php");
+
+?>
